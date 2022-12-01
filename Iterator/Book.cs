36b29f4383ddc809da20665d07dfcf3017dc16c7
TomoProg <helloworld0306.xxx@gmail.com>
2022-12-02 00:20:02 +0900

    本のとおりにiteratorパターンを実装した

---
+++ Iterator/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Iterator
+{
+    internal class Book
+    {
+        private string name;
+        public Book(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+    }
+}
--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -10,6 +10,21 @@ namespace Iterator
     {
         static void Main(string[] args)
         {
+            var bookShelf = new BookShelf(5);
+            bookShelf.appendBook(new Book("book1"));
+            bookShelf.appendBook(new Book("book2"));
+            bookShelf.appendBook(new Book("book3"));
+            bookShelf.appendBook(new Book("book4"));
+            bookShelf.appendBook(new Book("book5"));
+            bookShelf.appendBook(new Book("book6"));
+            var it = bookShelf.iterator();
+
+            while(it.hasNext())
+            {
+                var book = it.next() as Book;
+                Console.WriteLine(book.GetName());
+            }
+
         }
     }
 }
