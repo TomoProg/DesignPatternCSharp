c8682c8a69be17ac548df5bf4be6e5baa5a12397
TomoProg <helloworld0306.xxx@gmail.com>
2022-12-04 18:42:42 +0900

    フォルダ構成変更

---
+++ 01_Iterators/Iterator/BookShelfIterator.cs
@@ -0,0 +1,29 @@
+﻿namespace Iterator
+{
+    internal class BookShelfIterator : Iterator
+    {
+        private BookShelf bookShelf;
+        private int index = 0;
+
+        public BookShelfIterator(BookShelf bookShelf)
+        {
+            this.bookShelf = bookShelf;
+        }
+
+        /// <summary>
+        /// 次に返す本があるかどうか
+        /// </summary>
+        /// <returns></returns>
+        public bool hasNext()
+        {
+            return index < this.bookShelf.getLength();
+        }
+
+        public object next()
+        {
+            var book = this.bookShelf.getBookAt(index);
+            index++;
+            return book;
+        }
+    }
+}
