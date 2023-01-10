89ef0383d8e0c5e35b0f02a3065e70ac66da8412
TomoProg <t.yamamoto@neogenia.co.jp>
2023-01-10 22:24:51 +0900

    書籍の写経

---
+++ 06_Prototypes/Prototype/MessageBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Prototype.Framework;
+
+namespace Prototype
+{
+	public class MessageBox : Product
+	{
+        private char _decochar;
+
+		public MessageBox(char decochar)
+		{
+            _decochar = decochar;
+		}
+
+        public void Use(string s)
+        {
+            int length = System.Text.Encoding.UTF8.GetBytes(s).Length;
+            for(int i = 0; i < length + 4; i++)
+            {
+                Console.Write(_decochar);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"{_decochar} {s} {_decochar}");
+            for (int i = 0; i < length + 4; i++)
+            {
+                Console.Write(_decochar);
+            }
+            Console.WriteLine();
+        }
+
+        public Product CreateClone()
+        {
+            Product p = null;
+            try
+            {
+                p = (Product)MemberwiseClone();
+            }
+            catch(Exception e) // 書籍ではCloneNotSupportedExceptionだが、C#で対応するやつがどれか探すの面倒なので、とりあえずExceptionで受ける
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return p;
+        }
+    }
+}
+
---
+++ 06_Prototypes/Prototype/UnderlinePen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Prototype.Framework;
+
+namespace Prototype
+{
+    public class UnderlinePen : Product
+    {
+        private char _ulchar;
+
+        public UnderlinePen(char ulchar)
+        {
+            _ulchar = ulchar;
+        }
+
+        public void Use(string s)
+        {
+            Console.WriteLine($"\"{s}\"");
+            Console.Write(" ");
+            int length = System.Text.Encoding.UTF8.GetBytes(s).Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(_ulchar);
+            }
+            Console.WriteLine();
+        }
+
+        public Product CreateClone()
+        {
+            Product p = null;
+            try
+            {
+                p = (Product)MemberwiseClone();
+            }
+            catch (Exception e) // 書籍ではCloneNotSupportedExceptionだが、C#で対応するやつがどれか探すの面倒なので、とりあえずExceptionで受ける
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return p;
+        }
+    }
+}
+
