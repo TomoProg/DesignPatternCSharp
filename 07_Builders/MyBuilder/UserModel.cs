766643ffc25c469436435c4b80136917d45ebb10
TomoProg <t.yamamoto@neogenia.co.jp>
2023-01-18 10:36:20 +0900

    fix

--- 07_Builders/MyBuilder/UserModel.cs
+++ 07_Builders/MyBuilder/UserModel.cs
@@ -3,9 +3,6 @@ namespace MyBuilder
 {
 	public class UserModel : DefaultBuilder<UserModel>
 	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public string Blood { get; set; }
 		public DateTime CreateTime { get; set; }
 		public DateTime UpdateTime { get; set; }
 		public int MembershipFee { get; set; }
