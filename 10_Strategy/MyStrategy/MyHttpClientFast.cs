255a0cc07d508de2f3231533a825d889a56238e7
TomoProg <t.yamamoto@neogenia.co.jp>
2023-02-16 10:23:31 +0900

    IMyHttpClientを実装するクラスを増やした

---
+++ 10_Strategy/MyStrategy/MyHttpClientFast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace MyStrategy
+{
+    class MyHttpClientFast : IMyHttpClient
+    {
+        public string GetHTML(string url)
+        {
+            // TODO: もっと高速なHttpClientを使う
+            Console.WriteLine("MyHttpCilentFast");
+            var c = new HttpClient();
+            return c.GetStringAsync(url).Result;
+        }
+    }
+}
