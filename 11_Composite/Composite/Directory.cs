04f6c3f0ff6474b725d1cd2fb30839621c96db59
TomoProg <t.yamamoto@neogenia.co.jp>
2023-02-22 09:42:57 +0900

    add composit

---
+++ 11_Composite/Composite/Directory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Directory : Entry
+    {
+        private string _name;
+        private List<Entry> _directory = new List<Entry>();
+
+        public Directory(string name)
+        {
+            _name = name;
+        }
+
+        public override string GetName()
+        {
+            return _name;
+        }
+
+        public override int GetSize()
+        {
+            return _directory.Sum(x => x.GetSize());
+        }
+
+        public override Entry Add(Entry entry)
+        {
+            _directory.Add(entry);
+            return this;
+        }
+
+        public override void PrintList(string prefix)
+        {
+            Console.WriteLine($"{prefix}/{this}");
+            _directory.ForEach(x => x.PrintList($"{prefix}/{_name}"));
+        }
+    }
+}
---
+++ 11_Composite/Composite/Entry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public abstract class Entry
+    {
+        public abstract string GetName();
+        public abstract int GetSize();
+
+        public virtual Entry Add(Entry entry)
+        {
+            // 書籍ではFileTreatmentExceptionというのを使っているが
+            // Java専用？なのか良く分からんので、とりあえずNotImplementedExceptionにしておく
+            throw new NotImplementedException();
+        }
+
+        public void PrintList()
+        {
+            PrintList("");
+        }
+
+        // 書籍ではprotectedだけど、C#だと派生先で呼べない？？？
+        public abstract void PrintList(string prefix);
+
+        public override string ToString()
+        {
+            // テンプレートメソッドになってる
+            return $"{GetName()} ({GetSize()})";
+        }
+    }
+}
