c0489a827efae0f1559fbb0c5ce064e3a5214395
TomoProg <t.yamamoto@neogenia.co.jp>
2023-03-09 13:33:07 +0900

    update visitor

--- 13_Visitor/MyVisitor/Program.cs
+++ 13_Visitor/MyVisitor/Program.cs
@@ -21,42 +21,44 @@ namespace MyVisitor
     // Visitor
     public abstract class Visitor
     {
-        public abstract void Visit(User user);
-        public abstract void Visit(Busho busho);
+        public abstract void Visit(Model m);
+        public virtual void Visit(ElementList elementList)
+        {
+            foreach (var e in elementList._elementList)
+            {
+                e.Accept(this);
+            }
+        }
     }
 
     // ConcreateVisitor
     public class CsvVisitor : Visitor
     {
-        public override void Visit(User user)
+        private bool _firstFlag = true;
+        public override void Visit(Model m)
         {
-            Console.WriteLine($"{user.Id},{user.FirstName},{user.LastName}");
+            var properties = m.GetProperties();
+            if (_firstFlag)
+            {
+                Console.WriteLine(string.Join(",", properties.Keys));
+                _firstFlag = false;
+            }
+            Console.WriteLine(string.Join(",", properties.Values));
         }
 
-        public override void Visit(Busho busho)
-        {
-            Console.WriteLine($"{busho.Id},{busho.Name}");
-        }
     }
 
     // ConcreateVisitor
     public class JsonVisitor : Visitor
     {
-        public override void Visit(User user)
+        public override void Visit(Model m)
         {
-            Console.WriteLine($@"{{
-  id: {user.Id},
-  first_name: {user.FirstName},
-  last_name: {user.LastName}
-}}");
-        }
-
-        public override void Visit(Busho busho)
-        {
-            Console.WriteLine($@"{{
-  id: {busho.Id},
-  name: {busho.Name}
-}}");
+            Console.WriteLine("{");
+            foreach(var prop in m.GetProperties())
+            {
+                Console.WriteLine($"  {prop.Key}: {prop.Value}");
+            }
+            Console.WriteLine("}");
         }
     }
 
@@ -66,12 +68,10 @@ namespace MyVisitor
         void Accept(Visitor v);
     }
 
-    // ConcreateElement
-    public class Busho : Element
+    // 
+    public abstract class Model : Element
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
+        public abstract Dictionary<string, string> GetProperties();
         public void Accept(Visitor v)
         {
             v.Visit(this);
@@ -79,22 +79,41 @@ namespace MyVisitor
     }
 
     // ConcreateElement
-    public class User : Element
+    public class Busho : Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override Dictionary<string, string> GetProperties()
+        {
+            return new Dictionary<string, string> {
+                { "id", Id.ToString() },
+                { "name", Name.ToString() }
+            };
+        }
+    }
+
+    // ConcreateElement
+    public class User : Model
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public void Accept(Visitor v)
+        public override Dictionary<string, string> GetProperties()
         {
-            v.Visit(this);
+            return new Dictionary<string, string> {
+                { "id", Id.ToString() },
+                { "first_name", FirstName.ToString() },
+                { "last_name", LastName.ToString() },
+            };
         }
     }
 
     // ObjectStructure
     public class ElementList
     {
-        private List<Element> _elementList;
+        public List<Element> _elementList;
         public ElementList()
         {
             _elementList = new List<Element>();
@@ -107,10 +126,8 @@ namespace MyVisitor
 
         public void Accept(Visitor v)
         {
-            foreach (var e in _elementList)
-            {
-                e.Accept(v);
-            }
+            // ElementListの中ではループしない
+            v.Visit(this);
         }
     }
 }
