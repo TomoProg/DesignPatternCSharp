29a76c7eaa7dacbf5fff95dfa80f30e7dc2d53db
TomoProg <t.yamamoto@neogenia.co.jp>
2023-04-09 20:21:11 +0900

    draw2のような機能を実装してみた

--- 16_Mediator/MyMediator/Program.cs
+++ 16_Mediator/MyMediator/Program.cs
@@ -11,38 +11,44 @@ namespace MyMediator
         static void Main(string[] args)
         {
             var m = new GameMaster();
-            var p1 = new ManualPlayer(m, "Sato");
-            var p2 = new ManualPlayer(m, "Yamamoto");
-            var p3 = new ManualPlayer(m, "Nagata");
-
-            m.AddPlayer(p1);
-            m.AddPlayer(p2);
-            m.AddPlayer(p3);
             m.Run();
         }
     }
 
-    // Mediator役がいない
-    // Mediatorに必要なメソッドだけをinterfaceで定義するんだろうけど
-    // 今回の例だと、GameMasterからinterfaceを分離したとしても、そのinterfaceから他になにか作るという状況が思い浮かばない...。
-    // interfaceは仕様なので、なにか別のものを作るという状況がなければ作らないという想定が違うのか。
+    // Mediator役
+    // GameMasterからMediatorとして必要なものだけを分離
+    // Colleague役はこれだけ知っていればいい。
+    public interface GameMasterMediator
+    {
+        void CreatePlayer();
+        void PlayerChanged(string command);
+    }
 
     // ConcrateMediator役
-    public class GameMaster
+    public class GameMaster : GameMasterMediator
     {
-        private List<BasePlayer> playerList = new List<BasePlayer>();
-        private int playerTurn = 0;
-
-        public void AddPlayer(BasePlayer p)
-        {
-            playerList.Add(p);
-        }
+        private List<BasePlayer> _playerList = new List<BasePlayer>();
+        private int _playerTurn = 0;
 
         public void Run()
         {
+            // プレイヤーの作成
+            CreatePlayer();
+
             Console.WriteLine($"■■■ ゲーム開始 ■■■");
-            var p = playerList[0];
+            var p = _playerList[0];
             p.DoAction();
+            PlayerChanged()
+        }
+
+        // ここが同僚役（Colleague）を作成するところ
+        // 今後、このGameMasterを継承してプレイヤーの作成のやり方を変える
+        // 実際に名前を入力させたり、なんらかのシュミレーション用でランダムにプレイヤー選んだりとか
+        public virtual void CreatePlayer()
+        {
+            _playerList.Add(new ManualPlayer(this, "Sato"));
+            _playerList.Add(new ManualPlayer(this, "Yamamoto"));
+            _playerList.Add(new ManualPlayer(this, "Nagata"));
         }
 
         public void PlayerChanged(string command)
@@ -51,20 +57,28 @@ namespace MyMediator
             {
                 // passコマンド
                 // 何もせずに次の人のターンになる
-                playerTurn = (playerTurn + 1) % playerList.Count;
-                playerList[playerTurn].DoAction();
+                _playerTurn = (_playerTurn + 1) % _playerList.Count;
+                _playerList[_playerTurn].DoAction();
             }
             else if (command == "s")
             {
                 // skipコマンド
                 // 次の人をスキップして、その次の人のターンになる
-                playerTurn = (playerTurn + 2) % playerList.Count;
-                playerList[playerTurn].DoAction();
+                _playerTurn = (_playerTurn + 2) % _playerList.Count;
+                _playerList[_playerTurn].DoAction();
+            }
+            else if (command == "d2")
+            {
+                // Draw2コマンド
+                // 次の人のカードを2枚増やす
+                _playerTurn = (_playerTurn + 1) % _playerList.Count;
+                _playerList[_playerTurn].CardNum += 2;
+                _playerList[_playerTurn].DoAction();
             }
             else
             {
                 // 上記以外のコマンドの場合は無効なので、もう一度同じプレイヤーのターンになる
-                playerList[playerTurn].DoAction();
+                _playerList[_playerTurn].DoAction();
             }
         }
     }
@@ -72,36 +86,49 @@ namespace MyMediator
     // Colleague役
     public abstract class BasePlayer
     {
-        private GameMaster _master;
+        private GameMasterMediator _mediator;
         public string Name { get; private set; }
+        public int CardNum { get; set; } = 5;
 
-        public BasePlayer(GameMaster master, string name)
+        public BasePlayer(GameMasterMediator mediator, string name)
         {
-            _master = master;
+            _mediator = mediator;
             Name = name;
         }
 
         public void DoAction()
         {
+            // テンプレートメソッド
             Console.WriteLine($"--- {Name}のターンです ---");
+            ShowState();
             var command = Think();
-            _master.PlayerChanged(command);
+
+            // 考えたこと
+            // Playerが変わったというかターンを変えてと命令しているイメージ。
+            // Playerのなにかしらの属性が変わったことによるイベントではないので違和感がある。
+            _mediator.PlayerChanged(command);
         }
 
-        public virtual string Think()
+        protected virtual string Think()
         {
             throw new NotImplementedException();
         }
+
+        protected void ShowState()
+        {
+            Console.WriteLine($"CardNum: {CardNum}");
+        }
+
     }
 
     // ConcreateColleague役
     public class ManualPlayer : BasePlayer
     {
-        public ManualPlayer(GameMaster master, string name) : base(master, name)
+        public ManualPlayer(GameMasterMediator mediator, string name) : base(mediator, name)
         {
         }
 
-        public override string Think()
+        protected override string Think()
         {
             return Console.ReadLine();
         }
